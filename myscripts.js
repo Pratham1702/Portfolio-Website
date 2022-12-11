1e1ad64b78018ccd7261068ddf4af85d0299991d
Pratham Goyal <prathamgoyal171002@gmail.com>
2022-12-11 22:45:00 +0530

    Innitial commit

--- myscripts.js
+++ myscripts.js
@@ -11,3 +11,34 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+// --------------------Open and close menu---------------------
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
+
+// -----------------Contact form-----------------
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbx4l14VYL8pYQgCS7EpA0oGKYcvh7HxMY12bS_LZrDQizfqBuxJ38UuFWX7nMa0DtSD9w/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+      })
+      setTimeout(function(){
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+      .catch(error => console.error('Error!', error.message))
+  })
